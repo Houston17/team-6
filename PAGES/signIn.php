f823cdf264de89d0d6303ea03fee5f18e55e1c5b
cbrowne30 <chrisbrowne@Chriss-MacBook.local>
2017-10-13 22:27:41 -0500

    Sign in additions
    Added php to sign in page

--- PAGES/signIn.php
+++ PAGES/signIn.php
@@ -1,4 +1,34 @@
 <!DOCTYPE html>
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['inputEmail']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['inputPassword']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -12,7 +42,7 @@
   <body>
     
     <div class="container">    
-        <form class="form-signin">
+        <form class="form-signin" method="post">
             <h2 class="form-signin-heading">Please sign in</h2>
             <label for="inputEmail" class="sr-only">Email address</label>
             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
