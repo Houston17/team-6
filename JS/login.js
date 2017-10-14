f409d8c592cba4fad8b13a20ea67ca633d278b2c
Josiah D. Brown <brownjosiah09@gmail.com>
2017-10-14 04:16:55 -0500

    login

--- JS/login.js
+++ JS/login.js
@@ -1,38 +1,24 @@
 $(document).ready(function(){
 
-    $('#addEvents').click(function (){
-        console.log("sup");
-        $('#eventForm').show();
-        $('#addEvents').hide();
-
-        $('#name').val("");
-        $('#email').val("");
-        $('#password').val("");
-        $('#eventLocation').val("");
-
-        writeUserData("Tarun", "tarunt");
-    })
 
     $('#submitUser').click(function() {
         console.log("Hi");
         var name = $('#name').val();
         var email = $('#email').val();
         var password = $('#password').val();
-        var eventLocation = $('#eventLocation').val();
 
         var tr = "<tr>\
                     <td>" + name + "</td>\
                     <td>" + email + "</td>\
                     <td>" + password + "</td>\
-                    <td>" + eventLocation + "</td>\
                   </tr>"
         
         $("#table").append(tr);
         
         $('#eventForm').hide();
         $('#addEvents').show();
-        console.log(name + email + password + eventLocation);
-        writeNewPost(name, email, password, eventLocation);
+        console.log(name + email + password);
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch
     })
 
     function writeUserData(userId, name, email) {
@@ -41,22 +27,21 @@ $(document).ready(function(){
         });
     }
 
-    function writeNewPost(title, date, time, location) {
+    function writeNewPost(title, date, time) {
         // A post entry.
         var postData = {
           Name: title,
           Date: date,
-          Time: time,
-          Location: location,
-
+          Time: time
+		  
         };
       
         // Get a key for a new Post.
-        var newPostKey = firebase.database().ref().child('Events').push().key;
+        var newPostKey = firebase.database().ref().child('Users').push().key;
       
         // Write the new post's data simultaneously in the posts list and the user's post list.
         var updates = {};
-        updates['Events/' + title] = postData;
+        updates['Users/' + title] = postData;
       
         return firebase.database().ref().update(updates);
     }
