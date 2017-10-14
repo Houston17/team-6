7cec5ca285eb6b996fcb0b1d1e193476c3b743ab
Tarun Thummala <tarunt815@gmail.com>
2017-10-14 02:42:22 -0500

    im a god

--- JS/events.js
+++ JS/events.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
         $('#eventDate').val("");
         $('#eventTime').val("");
         $('#eventLocation').val("");
+
+        writeUserData("Tarun", "tarunt");
     })
 
     $('#submitEvents').click(function() {
@@ -30,5 +32,32 @@ $(document).ready(function(){
         $('#eventForm').hide();
         $('#addEvents').show();
         console.log(eventName + eventDate + eventTime + eventLocation);
+        writeNewPost(eventName, eventDate, eventTime, eventLocation);
     })
+
+    function writeUserData(userId, name, email) {
+        firebase.database().ref('Users/' + userId).set({
+          username: name,
+        });
+    }
+
+    function writeNewPost(title, date, time, location) {
+        // A post entry.
+        var postData = {
+          Name: title,
+          Date: date,
+          Time: time,
+          Location: location,
+
+        };
+      
+        // Get a key for a new Post.
+        var newPostKey = firebase.database().ref().child('Events').push().key;
+      
+        // Write the new post's data simultaneously in the posts list and the user's post list.
+        var updates = {};
+        updates['Events/' + title] = postData;
+      
+        return firebase.database().ref().update(updates);
+    }
 })
