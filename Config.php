8b0dcba2d9e968eec5c13099e04cb15038098b94
cbrowne30 <chrisbrowne@Chriss-MacBook.local>
2017-10-13 23:44:53 -0500

    Delete delete delete
    Stuff stuff

--- Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', '172.31.57.252');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '19910331');
-   define('DB_DATABASE', 'Htown');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
