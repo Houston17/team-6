567c13bfa4ea3df7c07aa4f088e0f777414236af
Tarun Thummala <tarunt815@gmail.com>
2017-10-14 01:02:13 -0500

    events just got an upgrade bois

--- JS/events.js
+++ JS/events.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     $('#addEvents').click(function() {
         console.log("Hi");
+        var eventName = $('#eventName').val();
+        var eventDate = $('#eventDate').val();
+        var eventTime = $('#eventTime').val();
+        var eventLocation = $('#eventLocation').val();
+
+        var tr = "<tr>\
+                    <td>" + eventName + "</td>\
+                    <td>" + eventDate + "</td>\
+                    <td>" + eventTime + "</td>\
+                    <td>" + eventLocation + "</td>\
+                  </tr>"
+        
+        $("#table").append(tr);
+
+        console.log(eventName + eventDate + eventTime + eventLocation);
     })
 })
